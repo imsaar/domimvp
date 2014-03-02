2b9086f29c4774fdf32ff130fe4b4dac34c6e26b
Prelang Builder <builder@prelang.com>
2014-03-02 20:58:48 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Domimvp::Application.routes.draw do
+  get "homes/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
